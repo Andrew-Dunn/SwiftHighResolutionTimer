2d5fcaf1206ed5afa57c66368a81eee2dafd0565
Andrew Dunn <andrew@dunn.name>
2018-02-01 23:49:24 +1100

    Added HRT code.

--- Sources/HighResolutionTimer/HighResolutionTimer.swift
+++ Sources/HighResolutionTimer/HighResolutionTimer.swift
@@ -1,3 +1,72 @@
-struct SwiftHighResolutionTimer {
-    var text = "Hello, World!"
+//
+// Copyright © Andrew Dunn, 2018
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin
+#endif
+
+fileprivate let NANOSECONDS_IN_A_SECOND: Double = 1_000_000_000
+
+/// A basic timing class that will let you know how much time has passed between two points in time.
+public class HighResolutionTimer {
+    #if os(Linux)
+    fileprivate typealias EpochType = timespec
+    #else
+    fileprivate typealias EpochType = UInt64
+    private var timeBaseInfo = mach_timebase_info_data_t()
+    private let ticksPerSecond: Double
+    #endif
+    
+    private var epoch: EpochType
+    
+    /// Constructor. Also sets the start time.
+    public init() {
+        #if os(Linux)
+            epoch = timespec()
+            clock_gettime(CLOCK_MONOTONIC, &epoch)
+        #else
+            mach_timebase_info(&timeBaseInfo)
+            ticksPerSecond = Double(timeBaseInfo.numer) / Double(timeBaseInfo.denom) / NANOSECONDS_IN_A_SECOND
+            epoch = mach_absolute_time()
+        #endif
+    }
+    
+    /// Set the start time.
+    public func mark() {
+        #if os(Linux)
+            clock_gettime(CLOCK_MONOTONIC, &epoch)
+        #else
+            epoch = mach_absolute_time()
+        #endif
+    }
+    
+    /// Check how many seconds have elapsed since the start time was set.
+    ///
+    /// - Returns: The amount of time elapsed since the start time was set in seconds.
+    public func check() -> Double {
+        #if os(Linux)
+            var now = timespec()
+            clock_gettime(CLOCK_MONOTONIC, &now)
+            let delta = Double(now.tv_sec - epoch.tv_sec)
+                + (Double(now.tv_nsec - epoch.tv_nsec) / NANOSECONDS_IN_A_SECOND)
+        #else
+            let delta = Double(mach_absolute_time() - epoch) * ticksPerSecond
+        #endif
+        return delta;
+    }
 }
--- Tests/HighResolutionTimerTests/HighResolutionTimerTests.swift
+++ Tests/HighResolutionTimerTests/HighResolutionTimerTests.swift
@@ -1,16 +1,43 @@
+//
+// Copyright © Andrew Dunn, 2018
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
 import XCTest
-@testable import SwiftHighResolutionTimer
+import HighResolutionTimer
 
-class SwiftHighResolutionTimerTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftHighResolutionTimer().text, "Hello, World!")
+class HighResolutionTimerTests: XCTestCase {
+    func testAccuracy() {
+        let hrt = HighResolutionTimer()
+        XCTAssertLessThan(hrt.check(), 0.001)
+        hrt.mark()
+        Thread.sleep(forTimeInterval: TimeInterval(2.0))
+        var elapsed = hrt.check()
+        XCTAssertGreaterThan(elapsed, 2.000)
+        XCTAssertLessThan(elapsed, 2.005)
+        hrt.mark()
+        Thread.sleep(forTimeInterval: TimeInterval(5.0))
+        elapsed = hrt.check()
+        XCTAssertGreaterThan(elapsed, 5.000)
+        XCTAssertLessThan(elapsed, 5.005)
+        hrt.mark()
+        Thread.sleep(forTimeInterval: TimeInterval(10.0))
+        elapsed = hrt.check()
+        XCTAssertGreaterThan(elapsed, 10.000)
+        XCTAssertLessThan(elapsed, 10.005)
     }
-
-
     static var allTests = [
-        ("testExample", testExample),
+        ("testAccuracy", testAccuracy),
     ]
 }
